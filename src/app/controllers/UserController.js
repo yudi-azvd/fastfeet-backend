554af70b5ad0e28febfc401af155f7f3f8e7f07c
Yudi Yamane <yudiyamane@gmail.com>
2020-01-31 00:31:35 -0300

    add yup validation to session and user controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,8 +6,12 @@ class UserController {
   async store(request, response) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
-      email: Yup.string().required(),
-      password: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
     });
 
     if (!(await schema.isValid(request.body))) {
@@ -28,7 +32,49 @@ class UserController {
   }
 
   async update(request, response) {
-    return response.json('ok');
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if (email && email !== user.email) {
+      const existingUser = await User.findOne({
+        where: { email: request.body.email },
+      });
+
+      if (existingUser) {
+        return response.status(400).json({ error: 'This email is being used' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response
+        .status(401)
+        .json({ error: 'Old password does not match' });
+    }
+
+    const { id, name } = await user.update(request.body);
+
+    return response.json({ name, email, id });
   }
 }
 
