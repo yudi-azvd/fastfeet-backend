a4c61aa78d083a368fd1727ac1385fe5207a880e
Yudi Yamane <yudiyamane@gmail.com>
2020-04-08 20:07:24 -0300

    added querying by id for recipient controller

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -4,8 +4,14 @@ import Recipient from '../models/Recipient';
 
 class RecipientController {
   async index(request, response) {
-    const { q = '' } = request.query;
+    const { q = '', id: recipientId = null } = request.query;
     let recipients = [];
+
+    if (recipientId) {
+      const recipient = await Recipient.findByPk(recipientId);
+
+      return response.json(recipient);
+    }
 
     if (q) {
       recipients = await Recipient.findAll({
