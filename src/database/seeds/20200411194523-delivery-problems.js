89081a96ea5568552ba71754be1bb3d54a20724d
Yudi Yamane <yudiyamane@gmail.com>
2020-04-11 17:00:51 -0300

    removed ESLint problems

--- src/database/seeds/20200411194523-delivery-problems.js
+++ src/database/seeds/20200411194523-delivery-problems.js
@@ -1,5 +1,5 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: queryInterface => {
     return queryInterface.bulkInsert(
       'delivery_problems',
       [
@@ -26,7 +26,7 @@ module.exports = {
     );
   },
 
-  down: (queryInterface, Sequelize) => {
+  down: queryInterface => {
     return queryInterface.bulkDelete('delivery_problems', null, {});
   },
 };
